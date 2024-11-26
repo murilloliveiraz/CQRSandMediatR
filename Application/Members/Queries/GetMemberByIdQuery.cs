9a7b37835a1a55ecce819e433e72de3ab36a9745
Murillo Oliveira <murillo1047@gmail.com>
2024-11-25 22:12:24 -0300

    feat: member queries

---
+++ Application/Members/Queries/GetMemberByIdQuery.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Members.Queries
+{
+    public class GetMemberByIdQuery : IRequest<Member>
+    {
+        public int Id { get; set; }
+        public class GetMemberByIdQueryHandler : IRequestHandler<GetMemberByIdQuery, Member>
+        {
+            private readonly IMemberDapperRepository _memberDapperRepository;
+
+            public GetMemberByIdQueryHandler(IMemberDapperRepository memberDapperRepository)
+            {
+                _memberDapperRepository = memberDapperRepository;
+            }
+            public async Task<Member> Handle(GetMemberByIdQuery request, CancellationToken cancellationToken)
+            {
+                var member = await _memberDapperRepository.GetMemberById(request.Id);
+                return member;
+            }
+        }
+    }
+}
---
+++ Application/Members/Queries/GetMembersQuery.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Abstractions;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Members.Queries
+{
+    public class GetMembersQuery: IRequest<IEnumerable<Member>>
+    {
+        public class GetMembersQueryHandler : IRequestHandler<GetMembersQuery, IEnumerable<Member>>
+        {
+            private readonly IMemberDapperRepository _repository;
+            public GetMembersQueryHandler(IMemberDapperRepository repository)
+            {
+                _repository = repository;
+            }
+            public async Task<IEnumerable<Member>> Handle(GetMembersQuery request, CancellationToken cancellationToken)
+            {
+                var members = await _repository.GetAll();
+                return members;
+            }
+        }
+    }
+}
