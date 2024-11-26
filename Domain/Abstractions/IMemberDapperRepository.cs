2dee1cdd11f5fe91b6ded9dda843d50468fb544a
Murillo Oliveira <murillo1047@gmail.com>
2024-11-25 22:13:16 -0300

    feat: adding dapper

---
+++ Domain/Abstractions/IMemberDapperRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Domain.Abstractions
+{
+    public interface IMemberDapperRepository
+    {
+        Task<IEnumerable<Member>> GetAll();
+        Task<Member> GetMemberById(int memberId);
+    }
+}
---
+++ Infraestructure/Repositories/MemberDapperRepository.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Domain.Abstractions;
+using Domain.Entities;
+using System.Data;
+
+namespace Infraestructure.Repositories
+{
+    public class MemberDapperRepository : IMemberDapperRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public MemberDapperRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task<IEnumerable<Member>> GetAll()
+        {
+            string query = "SELECT * FROM \"Members\"";
+            return await _dbConnection.QueryAsync<Member>(query);
+        }
+
+        public async Task<Member> GetMemberById(int memberId)
+        {
+            string query = "SELECT * FROM \"Members\" WHERE \"Id\" = @Id";
+            return await _dbConnection.QueryFirstOrDefaultAsync<Member>(query, new { Id = memberId });
+        }
+
+    }
+}
